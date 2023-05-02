27689b80e81ea85bdc152289652c1dcb789af2b5
RitaCarrilho <ritaclameira@gmail.com>
2023-05-02 14:54:36 +0200

    created servlet

---
+++ jeefirst/src/main/java/com/lidem/cfa/controller/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.lidem.cfa.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="HelloServlet", urlPatterns={"/hello"}) // route for controller
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>hello</title></head><body><h2>Hello Servlet!</h2></body></html>");
+    }
+}
---
+++ jeefirst/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! Miau miau le petit chat</h2>
+</body>
+</html>
